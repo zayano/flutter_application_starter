7a2116c2f7a0905d6954668b70382c44e30825e2
Reza Aditya <reza.aditya@pelni.co.id>
2025-05-05 18:51:50 +0700

    pub init

---
+++ lib/h.dart
@@ -0,0 +1,6 @@
+// Pub
+export 'package:flutter/material.dart';
+
+// Presentation
+export 'package:flutter_application_starter/presentation/app_widget.dart';
+export 'package:flutter_application_starter/presentation/home/home_page.dart';
